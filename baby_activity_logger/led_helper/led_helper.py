56ac233d3266d7fcfd1a7dc430883231e9bcfaaf
Josh Harsono <jharsono@gmail.com>
2020-01-27 22:16:03 -0800

    fix LED helper flickering

--- baby_activity_logger/led_helper/led_helper.py
+++ baby_activity_logger/led_helper/led_helper.py
@@ -21,7 +21,12 @@ class LEDHelper:
         self.red.start(0)
         self.green.start(0)
         self.blue.start(0)
-        rgb = [(x / 255.0) * 100 for x in rgb]
+
+        # The line below should allow you to use web-like RGB values from 0-255,
+        # but currently causes flickering with 470ohm resistors
+
+        # rgb = [(x / 255.0) * 100 for x in rgb]
+
         self.red.ChangeDutyCycle(rgb[0])
         self.green.ChangeDutyCycle(rgb[1])
         self.blue.ChangeDutyCycle(rgb[2])
@@ -32,10 +37,10 @@ class LEDHelper:
         self.blue.stop()
 
     def set_fetch_status(self):
-        self.set_color([255, 0, 255])
+        self.set_color([1, 0, 1])
 
     def set_success_status(self):
-        self.set_color([0, 255, 0])
+        self.set_color([0, 1, 0])
 
     def set_fail_status(self):
-        self.set_color([255, 0, 0])
+        self.set_color([1, 0, 0])
